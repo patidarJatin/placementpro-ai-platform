6403f127855e37d58fc2ed78090f94b9eb3edd59
patidarJatin <j830595p@gmail.com>
2026-07-24 15:08:00 +0530

    Initial project setup

---
+++ backend/src/main/java/com/jatinpatidar/placementpro/PlacementProApplication.java
@@ -0,0 +1,13 @@
+package com.jatinpatidar.placementpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacementProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacementProApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jatinpatidar/placementpro/PlacementproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jatinpatidar.placementpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacementproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
