6050b93bc646522a5decb1b1549ec117f4299ba6
patidarJatin <j830595p@gmail.com>
2026-07-26 16:49:20 +0530

    Create registration request DTO

---
+++ backend/src/main/java/com/jatinpatidar/placementpro/dto/RegisterRequest.java
@@ -0,0 +1,29 @@
+package com.jatinpatidar.placementpro.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 3, max = 100, message = "Name must be between 3 and 100 characters")
+    private String fullName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message ="Enter a valid email" )
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8,max = 20,message = "Password must be between 8 and 20 characters")
+    private String password;
+}
+
