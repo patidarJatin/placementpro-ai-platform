cff6f7f4272002b4115bf489c71b449b1176f315
patidarJatin <j830595p@gmail.com>
2026-07-29 17:41:06 +0530

    feat: implement global exception handling and validation error responses

--- backend/src/main/java/com/jatinpatidar/placementpro/exceptions/GlobalExceptionHandler.java
+++ backend/src/main/java/com/jatinpatidar/placementpro/exceptions/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.jatinpatidar.placementpro.dto.auth.response.ErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -25,6 +26,27 @@ public class GlobalExceptionHandler {
         );
                 return ResponseEntity
                 .status(HttpStatus.CONFLICT)
+                .body(errorResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(
+            MethodArgumentNotValidException exception ,HttpServletRequest request
+    ){
+        String message = exception.getBindingResult()
+                .getFieldErrors()
+                .get(0)
+                .getDefaultMessage();
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                message,
+                request.getRequestURI()
+        );
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
                 .body(errorResponse);
     }
 }
